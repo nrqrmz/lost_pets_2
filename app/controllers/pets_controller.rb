4e76cfc88f7eba11567732b4b1f8c918161ba66b
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-16 07:16:53 -0500

    add routes/controller for #update

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -22,6 +22,13 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
   end
 
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+
+    redirect_to pet_path(@pet)
+  end
+
   private
 
   def pet_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   post 'pets', to: 'pets#create'
   get 'pets/:id', to: 'pets#show', as: :pet
   get 'pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  patch 'pets/:id', to: 'pets#update'
 end
